b4a00e2236e3464cca358ff9c0ced56e31107aa2
Jett <jettc505@gmail.com>
2022-10-07 00:39:40 -0600

    added finctanalitey to teh button

---
+++ assets/script.js
@@ -0,0 +1,8 @@
+var startBtn = document.querySelector("#startQuizBtn");
+var startContent = document.querySelector(".startInfo");
+var questions = document.querySelector(".questions");
+
+startBtn.addEventListener("click", function(event) {
+    startContent.style.display = "none";
+    questions.style.display = "block";
+});
