23091df37e441e566eb56eb1b3a7b7b43cab4e69
Jett <jettc505@gmail.com>
2022-10-08 20:13:41 -0600

    steel working'

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,41 @@
 var startBtn = document.querySelector("#startQuizBtn");
 var startContent = document.querySelector(".startInfo");
-var questions = document.querySelector(".questions");
+var questionsPage = document.querySelector(".questionsPage");
+var question = document.querySelector("#question");
+var answerA = document.querySelector("#answerA");
+var answerB = document.querySelector("#answerB");
+var answerC = document.querySelector("#answerC");
+var answerD = document.querySelector("#answerD");
 
-startBtn.addEventListener("click", function(event) {
+var listOfQuestions = [questionsOne, questionsTwo, questionsThree, questionFour, questionFive];
+
+// Question 1
+var questionOne = "Commonly used data types DO NOT include?";
+var questionOneAnswers = ["String", "Boolean", "Number", "Alerts"];
+
+// Question 2
+var questionTwo = "The condition in an if/else statement is enclosed in enclosed with ____.";
+var questionTwoAnswers = ["Quotes", "Curly Brackets", "Parentheses", "Square Brackets"];
+
+// Question 3
+var questionThree = "Arrays In JavaScript can be used to store?";
+var questionThreeAnswers = ["Numbers and Strings", "Other Arrays", "Booleans", "All of the Above"];
+
+// Question 4
+var questionFour = "String values must be enclosed within___ when being assigned to variables?";
+var questionFourAnswers = ["Commas", "Curly Brackets", "Quotes", "Parentheses"];
+
+// Question 5
+var questionFive = "A very useful tool used during development and debugging for printing content to debugger is";
+var questionFiveAnswers = ["JavaScript", "Terminal/Bash", "For Loops", "Console Logs"];
+
+function getQuestion() {
+    var radques = Math.floor(Math.random() * listOfQuestions.length);
+}
+
+// when start button is clicked
+startBtn.addEventListener("click", function (event) {
     startContent.style.display = "none";
-    questions.style.display = "grid";
+    questionsPage.style.display = "grid";
+
 });
